b4644215cf97ef59721eeff7ea68dca11af909f3
Jason Tian <kuohuitien@gmail.com>
2021-11-05 09:25:16 +0800

    added evil

--- init.el
+++ init.el
@@ -30,3 +30,16 @@
       (package-install 'use-package)))
 ;;; use-package for all others
 (require 'use-package)
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+  ;;change the cursor color in terms of evil mode
+  (setq evil-emacs-state-cursor '("red" box))
+  (setq evil-normal-state-cursor '("green" box))
+  (setq evil-visual-state-cursor '("orange" box))
+  (setq evil-insert-state-cursor '("red" bar))
+  (setq evil-replace-state-cursor '("red" bar))
+  (setq evil-operator-state-cursor '("red" hollow))
+  )
