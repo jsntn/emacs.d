5c6607e384719b16f91e9b524d6c1b3280d614a9
Jason Tian <kuohuitien@gmail.com>
2021-11-05 13:26:39 +0800

    set use-package `:ensure` globally

--- init.el
+++ init.el
@@ -31,8 +31,9 @@
 ;;; use-package for all others
 (require 'use-package)
 
+(setq use-package-always-ensure t)
+
 (use-package evil
-  :ensure t
   :config
   (evil-mode 1)
   ;; change the cursor color in terms of evil mode
