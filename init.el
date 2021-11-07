7c282514f3f22d928589376a7e4b3d15cec541fd
Jason Tian <kuohuitien@gmail.com>
2021-11-08 00:47:14 +0800

    enabled save-place-mode

--- init.el
+++ init.el
@@ -290,6 +290,8 @@
 ;; others settings
 ;; ===============================================================
 
+(save-place-mode 1)
+
 (fset 'yes-or-no-p 'y-or-n-p) ;; use 'y/n' instead of 'yes/no'
 
 (setq confirm-kill-emacs
