7c315952d6d43a8a81869a9f60dd37c409806d2d
Jason Tian <kuohuitien@gmail.com>
2021-11-08 22:26:31 +0800

    added package - neotree

--- init.el
+++ init.el
@@ -127,6 +127,22 @@
 	 ("k" . evil-previous-line))
   )
 
+(use-package neotree
+  :config
+  (setq neo-smart-open t)
+  (setq neo-window-fixed-size nil)
+  (global-set-key [f8] 'neotree-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+  (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+  (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+  (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
+  )
+
 (use-package org-bullets
   :init
   (setq org-bullets-bullet-list
