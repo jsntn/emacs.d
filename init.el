64283a0af63c5a93458b59a85d413fa2576ef6fe
Jason Tian <kuohuitien@gmail.com>
2021-11-12 15:59:18 +0800

    reloading .dir-locals.el hook

--- init.el
+++ init.el
@@ -464,6 +464,32 @@ Version 2017-03-12"
 (define-key evil-insert-state-map (kbd "C-z") 'undo-fu-only-undo)
 (define-key evil-insert-state-map (kbd "C-y") 'undo-fu-only-redo)
 
+;; via https://emacs.stackexchange.com/questions/13080/reloading-directory-local-variables
+(defun my-reload-dir-locals-for-current-buffer ()
+  "reload dir locals for the current buffer"
+  (interactive)
+  (let ((enable-local-variables :all))
+    (hack-dir-local-variables-non-file-buffer)))
+
+(defun my-reload-dir-locals-for-all-buffer-in-this-directory ()
+  "For every buffer with the same `default-directory` as the
+current buffer's, reload dir-locals."
+  (interactive)
+  (let ((dir default-directory))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+	(when (equal default-directory dir))
+	(my-reload-dir-locals-for-current-buffer)))))
+
+(add-hook 'emacs-lisp-mode-hook
+	  (defun enable-autoreload-for-dir-locals ()
+	    (when (and (buffer-file-name)
+		       (equal dir-locals-file
+			      (file-name-nondirectory (buffer-file-name))))
+	      (add-hook 'after-save-hook
+			'my-reload-dir-locals-for-all-buffer-in-this-directory
+			nil t))))
+
 
 ;; ===============================================================
 ;; footer
