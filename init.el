b2d1356917df9ad023da84252d2b4cf099675594
Jason Tian <kuohuitien@gmail.com>
2021-11-12 23:00:28 +0800

    hook to remove the space between chinese characters during exporting to HTML files

--- init.el
+++ init.el
@@ -490,6 +490,33 @@ current buffer's, reload dir-locals."
 			'my-reload-dir-locals-for-all-buffer-in-this-directory
 			nil t))))
 
+(defun eh-org-clean-space (text backend info)
+  "Remove the space between chinese characters during exporting to HTML files."
+  ;; https://github.com/hick/emacs-chinese#%E4%B8%AD%E6%96%87%E6%96%AD%E8%A1%8C
+  (when (org-export-derived-backend-p backend 'html)
+    (let ((regexp "[[:multibyte:]]")
+	  (string text))
+      ;; Org converts line-break with space by default, remove this as this is not necessary for chinese characters
+      (setq string
+	    (replace-regexp-in-string
+	     (format "\\(%s\\) *\n *\\(%s\\)" regexp regexp)
+	     "\\1\\2" string))
+;;      ;; remove the space before the bold
+;;      (setq string
+;;	    (replace-regexp-in-string
+;;	     (format "\\(%s\\) +\\(<\\)" regexp)
+;;	     "\\1\\2" string))
+;;      ;; remove the space after the bold
+;;      (setq string
+;;	    (replace-regexp-in-string
+;;	     (format "\\(>\\) +\\(%s\\)" regexp)
+;;	     "\\1\\2" string))
+      string))
+  )
+(with-eval-after-load 'ox
+  (add-to-list 'org-export-filter-paragraph-functions 'eh-org-clean-space)
+  )
+
 
 ;; ===============================================================
 ;; footer
