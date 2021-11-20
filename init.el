d0af1128a9aeab5c82bd0055c683027387ea765b
Jason Tian <kuohuitien@gmail.com>
2021-11-21 01:13:01 +0800

    separate sessions settings from init.el

--- init.el
+++ init.el
@@ -437,52 +437,7 @@ Version 2017-03-12"
 
 (require 'init-font) ;; font settings
 (require 'init-keybindings) ;; keybindings with general.el
-
-;; =============================================================================
-;; session settings
-;; =============================================================================
-
-;; save and restore editor sessions between restarts
-
-;; save a list of open files in ~/.emacs.d/.emacs.desktop
-(setq desktop-path (list user-emacs-directory)
-      desktop-auto-save-timeout 600
-      desktop-save t
-      desktop-restore-eager 10 ;; the maximum number of 10 buffers to restore
-			       ;; immediately, and the remaining buffers are
-			       ;; restored lazily, when Emacs is idle.
-      )
-(desktop-save-mode 1)
-
-(add-hook 'emacs-startup-hook 'desktop-read)
-
-;; save a bunch of variables to the desktop file
-;; for lists specify the len of the maximal saved data also
-(setq desktop-globals-to-save
-      '((comint-input-ring        . 50)
-        (compile-history          . 30)
-        desktop-missing-file-warning
-        (dired-regexp-history     . 20)
-        (extended-command-history . 30)
-        (face-name-history        . 20)
-        (file-name-history        . 100)
-        (grep-find-history        . 30)
-        (grep-history             . 30)
-        (ivy-history              . 100)
-        (magit-revision-history   . 50)
-        (minibuffer-history       . 50)
-        (org-clock-history        . 50)
-        (org-refile-history       . 50)
-        (org-tags-history         . 50)
-        (query-replace-history    . 60)
-        (read-expression-history  . 60)
-        (regexp-history           . 60)
-        (regexp-search-ring       . 20)
-        register-alist
-        (search-ring              . 20)
-        (shell-command-history    . 50)
-        tags-file-name
-        tags-table-list))
+(require 'init-sessions) ;; session settings
 
 
 ;; =============================================================================
---
+++ lisp/init-sessions.el
@@ -0,0 +1,53 @@
+;;; init-sessions.el --- save and restore editor sessions between restarts -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+;; save a list of open files in ~/.emacs.d/.emacs.desktop
+(setq desktop-path (list user-emacs-directory)
+      desktop-auto-save-timeout 600
+      desktop-save t
+      desktop-restore-eager 10 ;; the maximum number of 10 buffers to restore
+			       ;; immediately, and the remaining buffers are
+			       ;; restored lazily, when Emacs is idle.
+      )
+(desktop-save-mode 1)
+
+(add-hook 'emacs-startup-hook 'desktop-read)
+
+;; save a bunch of variables to the desktop file
+;; for lists specify the len of the maximal saved data also
+(setq desktop-globals-to-save
+      '((comint-input-ring        . 50)
+        (compile-history          . 30)
+        desktop-missing-file-warning
+        (dired-regexp-history     . 20)
+        (extended-command-history . 30)
+        (face-name-history        . 20)
+        (file-name-history        . 100)
+        (grep-find-history        . 30)
+        (grep-history             . 30)
+        (ivy-history              . 100)
+        (magit-revision-history   . 50)
+        (minibuffer-history       . 50)
+        (org-clock-history        . 50)
+        (org-refile-history       . 50)
+        (org-tags-history         . 50)
+        (query-replace-history    . 60)
+        (read-expression-history  . 60)
+        (regexp-history           . 60)
+        (regexp-search-ring       . 20)
+        register-alist
+        (search-ring              . 20)
+        (shell-command-history    . 50)
+        tags-file-name
+        tags-table-list))
+
+
+(provide 'init-sessions)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; init-sessions.el ends here
