8102e600faa74e4bec7d15cbafd3f19fcd9a820b
Jason Tian <kuohuitien@gmail.com>
2022-10-14 21:36:39 +0800

    updated package-archives config

--- init.el
+++ init.el
@@ -71,8 +71,8 @@
 ;; =============================================================================
 
 (require 'package)
-(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
