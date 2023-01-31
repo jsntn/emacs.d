274b9e0d6bfce3dcb03f744c2fec4b9fc3ff36e3
Jason Tian <kuohuitien@gmail.com>
2023-01-31 13:54:22 +0800

    commented elpy

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -55,14 +55,14 @@
 
 (use-package elpa-mirror)
 
-(use-package elpy
-  :config
-  (elpy-enable)
-  ;; use flycheck instead of flymake
-  (when (load "flycheck" t t)
-    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-    (add-hook 'elpy-mode-hook 'flycheck-mode))
-  )
+;; (use-package elpy
+;;   :config
+;;   (elpy-enable)
+;;   ;; use flycheck instead of flymake
+;;   (when (load "flycheck" t t)
+;;     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+;;     (add-hook 'elpy-mode-hook 'flycheck-mode))
+;;   )
 
 (use-package evil
   :init
