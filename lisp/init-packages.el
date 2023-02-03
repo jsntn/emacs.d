aa238662bf4cf9187d875d10524190abab97f72d
Jason Tian <kuohuitien@gmail.com>
2023-02-03 12:58:58 +0800

    removed some config from workgroups2

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -299,24 +299,6 @@ You need to install it manually. Continue?")
 
   (workgroups-mode 1)
 
-  ;; what to do on Emacs exit / workgroups-mode exit?
-  (setq wg-emacs-exit-save-behavior 'save) ; options: 'save 'ask nil
-  (setq wg-workgroups-mode-exit-save-behavior 'save) ; options: 'save 'ask nil
-  ;; wg-save-session after after-save-hook
-  ;; https://github.com/pashinin/workgroups2/issues/93
-  ;; https://github.com/pashinin/workgroups2/issues/115
-  ;; to be tested...
-  (defun after-save-hook-setup ()
-    (wg-save-session))
-  (add-hook 'after-save-hook 'after-save-hook-setup)
-  
-  ;; auto save once kill emacs
-  ;; to be tested...
-  ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/emacsclient-Options.html
-  ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Killing-Emacs.html
-  (add-hook 'kill-emacs-hook '(wg-save-session "last-wg"))
-
-  ;; mode line changes
   ;; display workgroups in mode line
   (setq wg-mode-line-display-on t) ; default: (not (featurep 'powerline))
   )
