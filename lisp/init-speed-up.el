61744b73ad178a26b769ec9c3cad03c9bd558fce
Jason TIAN <jsntn@users.noreply.github.com>
2023-03-01 20:01:47 +0800

    collect garbage only when idle for 25s

--- lisp/init-speed-up.el
+++ lisp/init-speed-up.el
@@ -7,6 +7,10 @@
 ;; https://github.com/doomemacs/doomemacs/issues/3108#issuecomment-627537230
 ;; (setq garbage-collection-messages t)
 
+;; collect garbage only when idle for 15s
+;; https://emacs.stackexchange.com/q/34342
+(run-with-idle-timer 15 t (lambda () (garbage-collect)))
+
 ;; http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold
 (defun my/minibuffer-setup-hook ()
   (setq gc-cons-threshold most-positive-fixnum))
