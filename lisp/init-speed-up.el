72620be83740258fdc73c9a2abed72309e6e3a0d
Jason TIAN <jsntn@users.noreply.github.com>
2023-03-01 20:55:15 +0800

    gc-cons-threshold to 16mb for minibuffer-exit-hook

--- lisp/init-speed-up.el
+++ lisp/init-speed-up.el
@@ -7,16 +7,17 @@
 ;; https://github.com/doomemacs/doomemacs/issues/3108#issuecomment-627537230
 ;; (setq garbage-collection-messages t)
 
-;; collect garbage only when idle for 15s
-;; https://emacs.stackexchange.com/q/34342
-(run-with-idle-timer 15 t (lambda () (garbage-collect)))
-
 ;; http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold
 (defun my/minibuffer-setup-hook ()
   (setq gc-cons-threshold most-positive-fixnum))
 
 (defun my/minibuffer-exit-hook ()
-  (setq gc-cons-threshold 800000))
+  ;; defer it so that commands launched immediately after will enjoy the
+  ;; benefits.
+  ;; https://github.com/doomemacs/doomemacs/blob/develop/docs/faq.org#how-does-doom-start-up-so-quickly
+  (run-at-time
+   1 nil (lambda () (setq gc-cons-threshold 16777216))) ; 16mb
+   )
 
 (add-hook 'minibuffer-setup-hook #'my/minibuffer-setup-hook)
 (add-hook 'minibuffer-exit-hook #'my/minibuffer-exit-hook)
