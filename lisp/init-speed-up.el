1f45592da98b49eba0302ec7114c263c2f9f5365
Jason Tian <kuohuitien@gmail.com>
2023-03-02 10:10:37 +0800

    formatted

--- lisp/init-speed-up.el
+++ lisp/init-speed-up.el
@@ -10,15 +10,17 @@
 ;; http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold
 (defun my/minibuffer-setup-hook ()
   ;; http://clhs.lisp.se/Body/v_most_p.htm
-  (setq gc-cons-threshold most-positive-fixnum))
+  (setq gc-cons-threshold most-positive-fixnum)
+  )
 
 (defun my/minibuffer-exit-hook ()
   ;; defer it so that commands launched immediately after will enjoy the
   ;; benefits.
   ;; https://github.com/doomemacs/doomemacs/blob/develop/docs/faq.org#how-does-doom-start-up-so-quickly
-  (run-at-time
-   1 nil (lambda () (setq gc-cons-threshold 16777216))) ; 16mb
-   )
+  (run-at-time 1 nil
+	       (lambda () (setq gc-cons-threshold 16777216)) ; 16mb
+	       )
+  )
 
 (add-hook 'minibuffer-setup-hook #'my/minibuffer-setup-hook)
 (add-hook 'minibuffer-exit-hook #'my/minibuffer-exit-hook)
