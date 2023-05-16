c45af99a580b6af85d94c4b2521cc0001f7a5220
Jason Tian <kuohuitien@gmail.com>
2023-05-16 18:17:21 +0800

    removed org version checking

--- test-startup.sh
+++ test-startup.sh
@@ -8,8 +8,6 @@ ${EMACS:=emacs} -nw --batch \
                               (user-emacs-directory default-directory)
                               (user-init-file (expand-file-name "init.el"))
                               (load-path (delq default-directory load-path)))
-                           (message "%s" (org-version))
                            (load-file user-init-file)
-                           (message "%s" (org-version))
                            (run-hooks (quote after-init-hook))))'
 echo "Startup successful"
