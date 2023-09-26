f730b0ac90c9f1ecda8c01dad6cf9e9cf80b8cd3
Jason TIAN <jsntn@users.noreply.github.com>
2023-09-26 22:59:57 +0800

    Remove unnecessary code in init-pre.el

--- lisp/init-pre.el
+++ lisp/init-pre.el
@@ -55,7 +55,6 @@ Version: 2023-09-24"
 	     do (set var (if (stringp value)
 	                        value
                           (eval value))))))
-	     do (set var (eval value)))))
 
 
 (defun my-check-for-executable (executable-name executable-file &optional message)
