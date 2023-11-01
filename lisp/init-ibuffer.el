2c7017a0bb63752f74afd71bc98f755cba5282a2
Jason Tian <kuohuitien@gmail.com>
2023-11-01 03:33:46 +0000

    chore: use human readable size column, and update ibuffer-formats

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -9,6 +9,19 @@
   (ibuffer-jump-to-buffer (buffer-name (cadr (buffer-list)))))
 (add-hook 'ibuffer-hook #'ibuffer-jump-to-last-buffer)
 
+(with-eval-after-load 'ibuffer
+  ;; use human readable size column instead of original one
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (file-size-human-readable (buffer-size))))
+
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 35 35 :left :nil) " "
+              (size-h 9 -1 :right) " "
+              (mode 12 12 :left :elide) " "
+              filename-and-process)))
+
 
 (provide 'init-ibuffer)
 
