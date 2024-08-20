96852f7c4719732accca69209ac97f4452546a48
Jason Tian <kuohuitien@gmail.com>
2024-08-20 10:12:08 +0800

    formatted and added message

--- lisp/init-gpg.el
+++ lisp/init-gpg.el
@@ -14,16 +14,17 @@
 Version 2023-08-04"
   (interactive)
   (let* ((link-info (org-element-context))
-         (path (org-element-property :path link-info))
-         (abs-path (if (string-prefix-p "file:" path)
-                       (file-truename (replace-regexp-in-string "^file:" "" path))
-                     (file-truename path)))
-         (default-decrypt-path (concat abs-path ".clear")))
+	 (path (org-element-property :path link-info))
+	 (abs-path (if (string-prefix-p "file:" path)
+		       (file-truename (replace-regexp-in-string "^file:" "" path))
+		     (file-truename path)))
+	 (default-decrypt-path (concat abs-path ".clear")))
     (if (file-exists-p abs-path)
-        (let ((decrypt-path (read-file-name
+	(let ((decrypt-path (read-file-name
 			     (format "Enter target path (default %s): " default-decrypt-path)
 			     nil nil nil default-decrypt-path)))
-          (epa-decrypt-file abs-path decrypt-path))
+	  (epa-decrypt-file abs-path decrypt-path)
+	  (message "%s is decrypted to %s" abs-path decrypt-path))
       (message "File not found: %s" abs-path))))
 ;; END }}
 
