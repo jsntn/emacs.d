7892b6bca80db8972d435592c6071f6d425627f9
Jason Tian <kuohuitien@gmail.com>
2025-04-02 12:11:53 +0800

    require reformatter and update its config

--- lisp/init-reformatter.el
+++ lisp/init-reformatter.el
@@ -4,22 +4,28 @@
 
 
 ;; START: reformatter config
-(use-package reformatter
-  :config
-  (reformatter-define css-yaml-format
-    :program "prettier"
-    :args (list "--write" buffer-file-name)
-    ;; https://emacs.stackexchange.com/questions/24298/can-i-eval-a-value-in-quote
-    )
-  (reformatter-define sh-format
-    :program "shfmt"
-    :args (list "-l" "-w" "-i" "4" buffer-file-name)
-    ;; 4 spaces as indent, read more https://github.com/mvdan/sh/blob/master/cmd/shfmt/shfmt.1.scd
-    ;; https://emacs.stackexchange.com/questions/24298/can-i-eval-a-value-in-quote
-    )
-  )
+(require 'reformatter)
+
 (my-check-for-executable "Prettier" "prettier")
+(reformatter-define my/css-yaml-format
+  :program "prettier"
+  :args (list "--write" buffer-file-name)
+  ;; https://emacs.stackexchange.com/questions/24298/can-i-eval-a-value-in-quote
+  )
+
 (my-check-for-executable "shfmt" "shfmt")
+(reformatter-define my/sh-format
+  :program "shfmt"
+  :args (list "-i" "4")
+  ;; 4 spaces as indent, read more https://github.com/mvdan/sh/blob/master/cmd/shfmt/shfmt.1.scd
+  ;; https://emacs.stackexchange.com/questions/24298/can-i-eval-a-value-in-quote
+  )
+
+(my-check-for-executable "Black" "black")
+(reformatter-define my/python-format
+  :program "black"
+  :args '("-"))
+
 ;; END: reformatter config
 
 
