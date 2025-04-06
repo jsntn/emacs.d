7da4952596894c719da82a29353499d941695269
Jason Tian <kuohuitien@gmail.com>
2025-04-06 17:27:06 +0800

    require pyvenv

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -5,23 +5,20 @@
 
 
 
-(use-package pyvenv
-  :config
-  ;; (pyvenv-mode t)
-
-  ;; set correct Python interpreter
-  (setq pyvenv-post-activate-hooks
-	(list (lambda ()
-		(if (equal system-type 'windows-nt)
-		    (setq python-shell-interpreter (concat pyvenv-virtual-env "Scripts/python"))
-		  (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python"))
-		  )
-		)))
-  (setq pyvenv-post-deactivate-hooks
-	(list (lambda ()
-		(setq python-shell-interpreter "python")
-		)))
-  )
+(require 'pyvenv)
+;; (pyvenv-mode t)
+;; set correct Python interpreter
+(setq pyvenv-post-activate-hooks
+      (list (lambda ()
+	      (if (equal system-type 'windows-nt)
+		  (setq python-shell-interpreter (concat pyvenv-virtual-env "Scripts/python"))
+		(setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python"))
+		)
+	      )))
+(setq pyvenv-post-deactivate-hooks
+      (list (lambda ()
+	      (setq python-shell-interpreter "python")
+	      )))
 
 
 (defun my/python-mode-config ()
