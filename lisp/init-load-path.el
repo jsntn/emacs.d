7e25beec36a094532ed06fd6446cb2823b108678
Jason Tian <kuohuitien@gmail.com>
2025-04-22 23:55:52 +0800

    add site-lisp and its subdirs to load-path

--- lisp/init-load-path.el
+++ lisp/init-load-path.el
@@ -3,43 +3,13 @@
 ;;; Code:
 
 
-;; cl - Common Lisp Extension
-(require 'cl-lib) ; https://emacs.stackexchange.com/questions/48109/require-cl-or-require-cl-lib
 
-(defun sanityinc/add-subdirs-to-load-path (parent-dir)
-  "Add every non-hidden subdir of PARENT-DIR to `load-path'."
-  (let ((default-directory parent-dir))
-    (setq load-path
-          (append
-           (cl-remove-if-not
-            #'file-directory-p
-            (directory-files (expand-file-name parent-dir) t "^[^\\.]"))
-           load-path))))
-
-;; add both site-lisp and its immediate subdirs to `load-path'
+;; add both site-lisp and its subdirs to `load-path'
 (push site-lisp-dir load-path)
-(sanityinc/add-subdirs-to-load-path site-lisp-dir)
 
-
-;; TODO: to be updated...
-;; for `magit special structure
-(let ((magit-lisp-dir (expand-file-name "magit/lisp/" site-lisp-dir)))
-  (push magit-lisp-dir load-path))
-;; for `transient special structure
-(let ((transient-lisp-dir (expand-file-name "transient/lisp/" site-lisp-dir)))
-  (push transient-lisp-dir load-path))
-;; for `notmuch special structure
-(let ((notmuch-emacs-dir (expand-file-name "notmuch/emacs/" site-lisp-dir)))
-  (push notmuch-emacs-dir load-path))
-;; for `org-roam special structure
-(let ((org-roam-extentions-dir (expand-file-name "org-roam/extensions/" site-lisp-dir)))
-  (push org-roam-extentions-dir load-path))
-;; for `with-editor special structure
-(let ((with-editor-lisp-dir (expand-file-name "with-editor/lisp/" site-lisp-dir)))
-  (push with-editor-lisp-dir load-path))
-;; for `workgroups2 special structure
-(let ((workgroups2-lisp-dir (expand-file-name "workgroups2/src/" site-lisp-dir)))
-  (push workgroups2-lisp-dir load-path))
+(let ((default-directory site-lisp-dir))
+  (normal-top-level-add-subdirs-to-load-path))
+;; https://web.archive.org/web/20250422152913/https://manateelazycat.github.io/2022/03/02/emacs-load-directory-recursively/
 
 
 
