d5b9eb51d579c98212cb8e1142534e7dc6f78aab
Jason TIAN <jsntn@users.noreply.github.com>
2025-04-27 19:17:47 +0800

    Remove straight.el config and its version listing from startup script

--- test-startup.sh
+++ test-startup.sh
@@ -10,18 +10,6 @@ ${EMACS:=emacs} -nw --batch \
                               (load-path (delq default-directory load-path)))
                            (load-file user-init-file)
                            (my/list-packages-and-versions)
-                           (straight-freeze-versions)
                            (run-hooks (quote after-init-hook))))'
-
-ls -lrt ./straight/versions
-
-directory="./straight/versions"
-for file in "$directory"/*
-do
-  if [ -f "$file" ]; then
-    cat "$file"
-    echo "-------------------"
-  fi
-done
 
 echo "Startup successful"
