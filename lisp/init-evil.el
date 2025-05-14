6562aab50feae1bc96e5ca4048585a25ed9f3293
Jason Tian <kuohuitien@gmail.com>
2025-05-14 10:16:43 +0800

    prevent the cursor state settings from applying to read-only buffers

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -43,8 +43,12 @@
       (setq evil-replace-state-cursor '("red" bar))
       (setq evil-operator-state-cursor '("red" hollow)))
   (progn
-    (add-hook 'evil-insert-state-entry-hook (lambda () (send-string-to-terminal "\033[5 q")))
-    (add-hook 'evil-insert-state-exit-hook (lambda () (send-string-to-terminal "\033[2 q")))
+    (add-hook 'evil-insert-state-entry-hook (lambda ()
+					      (unless buffer-read-only
+						(send-string-to-terminal "\033[5 q"))))
+    (add-hook 'evil-insert-state-exit-hook (lambda ()
+					     (unless buffer-read-only
+					       (send-string-to-terminal "\033[2 q"))))
     ;; via https://www.reddit.com/r/emacs/comments/4rd44a/how_to_change_cursor_in_evil_mode/
     (send-string-to-terminal "\033[2 q") ; the default cursor state
     )
